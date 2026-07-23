cf33a22b30a600dfdf30a757a4d0ea4b0583db76
Prodip5850 <prodipsarker361@gmail.com>
2026-07-23 21:41:44 +0600

    2nd

--- lib/Weather_screen.dart
+++ lib/Weather_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class WeatherScreen extends StatelessWidget {
@@ -14,27 +16,159 @@ class WeatherScreen extends StatelessWidget {
         centerTitle: true,
         actions: [IconButton(onPressed: () {}, icon: Icon(Icons.refresh))],
       ),
-      body: Column(
-        children: [
-          //Main card
-          Card(
-            child: Column(
-              children: [
-                Text(
-                  '380 F',
-                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //Main card
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
                 ),
-              ],
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                    child: const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            '380 F',
+                            style: TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 16),
+                          Icon(Icons.cloud, size: 64),
+                          SizedBox(height: 16),
+                          Text('Rain', style: TextStyle(fontSize: 20)),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ),
-          ),
-          const SizedBox(height: 20),
+            const SizedBox(height: 20),
+            const Text(
+              'Weather Forcust ',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 16),
 
-          //Weather Forcuste
-          const Placeholder(fallbackHeight: 150),
-          const SizedBox(height: 20),
-          //Information card
-          const Placeholder(fallbackHeight: 150),
-        ],
+            const SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  HourlyForcust(),
+                  HourlyForcust(),
+                  HourlyForcust(),
+                  HourlyForcust(),
+                  HourlyForcust(),
+                ],
+              ),
+            ),
+
+            //Weather Forcuste
+            const SizedBox(height: 20),
+            //Information card
+            Text(
+              'Additional Information',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                children: [
+                  Column(
+                    children: [
+                      Icon(Icons.water_drop, size: 24),
+                      SizedBox(height: 8),
+
+                      Text('Humidity'),
+                      SizedBox(height: 8),
+                      Text(
+                        '94',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(width: 70),
+
+                  Column(
+                    children: [
+                      Icon(Icons.wind_power, size: 24),
+                      SizedBox(height: 8),
+                      Text('Wind Speed'),
+                      SizedBox(height: 8),
+                      Text(
+                        '7.46',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(width: 70),
+                  Column(
+                    children: [
+                      Icon(Icons.umbrella, size: 24),
+                      SizedBox(height: 8),
+                      Text('Pressur'),
+                      SizedBox(height: 8),
+                      Text(
+                        '1006',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HourlyForcust extends StatelessWidget {
+  const HourlyForcust({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      child: Container(
+        width: 100,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(12)),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: const Column(
+            children: [
+              Text(
+                '3:40',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              Icon(Icons.cloud, size: 32),
+              SizedBox(height: 8),
+              Text('320.45'),
+            ],
+          ),
+        ),
       ),
     );
   }
